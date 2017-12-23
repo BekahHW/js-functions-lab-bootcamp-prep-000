43b20a1715858b4e087697d9e6df78035988196d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2017-12-23 16:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 
 function happyHolidays()
 {
-  console.log('Happy holidays!')
+  return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name)
 {
-  console.log('Happy holidays, ${name}!')
+ return 'Happy holidays, ${name}!'
 }
 
 function happyHolidayTo(holiday, name)
